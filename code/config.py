d9ec7bb47df5a853d148d6f7aa0ac513e271a306
howie <howiemac@gmail.com>
2015-01-20 18:32:48 +0000

    git and config 2 tidy up

--- code/config.py
+++ code/config.py
@@ -4,22 +4,18 @@ config file for app single-app server
 """
 
 sitename='deepian'
+meta_description="go beyond"
+meta_keywords="spiritual,spirituality,soul,enlightenment"
 
 #permits={} #basis of permit system
 
 guests=True #do guests have access by default?
-guest_comments=False
 ratings="admin"
-meta_description="go beyond"
-meta_keywords="spiritual,spirituality,soul,enlightenment"
-
 attribution="minimal" #attribution=False
 chronological=False
 default_class="Page"
-
 registration_method="admin" # "admin" : admin has to register each user                                                                    
                               # "approve" : online self registration with approval by admin 
-
 
 page_default=4031 #home page is deepian blog
 
--- code/serve.py
+++ code/serve.py
@@ -1,5 +1,5 @@
 """
-deep app server script
+evoke app server script
 
 this is for serving a single app
 
@@ -16,7 +16,7 @@ from twisted.application import service
 from base.serve import start
 
 ## Twisted requires the creation of the root-level application object to take place in this file
-application = service.Application("deepserver application")
+application = service.Application("evoke application")
 ## stitch it all together...
 start(application)
 
